9be2d2c3afd4197e159cd861cc5afbaf7f1be0a6
shre vish <shrevish95@gmail.com>
2019-08-03 16:10:33 +0530

    added post resource

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe "can created Post" do
+    before do
+      @post = Post.create(date: Date.today, rationale: "Test description")
+    end
+
+    it "can be created" do
+      expect(@post).to be_valid
+    end
+
+    it "cannot be created without date and rationale" do
+      @post.date = nil
+      @post.rationale = nil
+
+      expect(@post).to_not be_valid
+    end
+  end
+end
